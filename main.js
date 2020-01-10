1ff57fe3dc5408de7cdcf74d136c3a1d6858ca31
Ryan Yen <ryan.kho.yen@gmail.com>
2020-01-10 02:12:03 -0500

    Updated Website to include about page, made some finishing touches on home page.

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+ // executes when HTML-Document is loaded and DOM is ready
+  
+
+  $( ".card" ).hover(
+  function() {
+    $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
+  }, function() {
+    $(this).removeClass('shadow-lg');
+  }
+);
+
+
+
+
+  
+// document ready  
+});
